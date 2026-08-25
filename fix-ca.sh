29a1b3c36b4dca6c6def3a9e28f6493cd47174b6
rupandas8811-ship-it <rupandas8811@gmail.com>
2026-08-25 16:49:12 +0530

    chore: remove temporary migration and fix scripts

--- fix-ca.sh
+++
@@ -1,2 +0,0 @@
-sed -i 's/const updates: Partial<Production> = {/const updates: any = {/g' src/components/ProductionModule.tsx
-sed -i 's/client_communication_proof: caCommunicationProof/client_communication_proof: caCommunicationProof,/g' src/components/ProductionModule.tsx
--- patch_pushupdate.sh
+++
@@ -1,1 +0,0 @@
-sed -i.bak -e '1655,1673d' src/components/RoleContext.tsx
